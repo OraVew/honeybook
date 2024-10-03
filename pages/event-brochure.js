5d72dae35b29ae0ae65de8989c391d151c3112a6
oravew <liv@oravew.com>
2024-10-02 19:30:44 -0500

    button

--- pages/event-brochure.js
+++ pages/event-brochure.js
@@ -166,7 +166,7 @@ export default function BuildEventBrochure() {
             onClick={handleShareViaText}
             className="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
           >
-            Share via Text
+            Share Page via Text
           </button>
         </div>
         </section>
