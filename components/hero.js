ed06a3c353fda243ced441f1bcbfa77721d30a0b
oravew <liv@oravew.com>
2024-11-18 21:26:57 -0600

    push

--- components/hero.js
+++ components/hero.js
@@ -76,7 +76,7 @@ export default function Hero() {
       <div className="relative z-10 flex flex-col items-center justify-center h-full bg-opacity-50 bg-gray-800">
         <div className="text-center">
           <p className="text-lg font-semibold" style={{ color: "#D69600" }}>
-            A boutique event and venue space in Goose Island Chicago
+            A boutique event and venue space in Chicago
           </p>
           <h1 className="mt-4 text-6xl font-bold text-white">
             Design your custom event without the stress.
